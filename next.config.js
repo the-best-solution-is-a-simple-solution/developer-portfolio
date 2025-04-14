6f676a42a057160c4102fb827ce5c2473659dbd9
Aron <szabo5144@saskpolytech.ca>
2025-04-14 12:22:18 -0600

    Trying with .nojekyll option

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
     output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/developer-portfolio' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/developer-portfolio/' : '',
+    basePath: '/developer-portfolio',
+    assetPrefix: '/developer-portfolio/',
     reactStrictMode: true,
     sassOptions: {
         includePaths: [require('path').join(__dirname, 'css')], // Changed from 'styles' to 'css'
